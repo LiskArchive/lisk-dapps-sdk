52f5e941054a4f78755e67c884331238c115f3b5
landgraf.paul <landgraf.paul@gmail.com>
2015-04-20 21:07:06 +0200

    fix

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
-/*var codius = process.binding('async'),
+var codius = process.binding('async'),
 	routes = require('./api/routes.js'),
 	jayson = require('jayson'),
 	config = require('./config.json');
 
 var server = jayson.server({
-	api: function(route, method, body, callback) {
+	api: function (path, method, body, callback) {
 		var router = routes.filter(function (r) {
-			return (r.route == route && r.method == method);
+			return (r.path == path && r.method == method);
 		});
 
-		router(body, function (result) {
-			return callback(null, result);
+		if (!router) {
+			return callback("Router not found");
+		}
+		
+		router.router(body, function (result) {
+			callback(null, result);
 		});
 	}
 });
 
-console.log(config.jayson_port)
 server.http().listen(config.jayson_port);
 console.log("launched");
-*/
-
-var http = require('http');
-
-var server = http.createServer(function(req, res){
-});
-
-server.listen(8000);
