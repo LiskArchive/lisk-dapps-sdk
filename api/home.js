3cbcbb68099b944c833163b89cad1cce299dd1e3
Boris Povod <boris@crypti.me>
2015-04-22 03:59:31 +0300

    removed jayson

--- api/home.js
+++ api/home.js
@@ -1,4 +1,5 @@
 module.exports = function (body, cb) {
+	console.log("Hello, world!");
 	return cb({
 		success: true,
 		message: "Hello, world!"
--- index.js
+++ index.js
@@ -1,23 +1,5 @@
 var codius = process.binding('async'),
 	routes = require('./api/routes.js'),
-	jayson = require('jayson'),
 	config = require('./config.json');
 
-var server = jayson.server({
-	api: function (path, method, body, callback) {
-		var router = routes.filter(function (r) {
-			return (r.path == path && r.method == method);
-		});
-
-		if (!router) {
-			return callback("Router not found");
-		}
-
-		router.router(body, function (result) {
-			callback(null, result);
-		});
-	}
-});
-
-server.http().listen(config.jayson_port);
-console.log("launched");
+console.log("Hello, world!");
