87eb518905eddac1eb56eba23e4276a35c82afd9
Boris Povod <boris@crypti.me>
2015-04-23 16:06:10 +0300

    test

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ var codius = process.binding('async'),
 	routes = require('./api/routes.js'),
 	config = require('./config.json');
 
-setInterval(function () {
-	console.log("dapp working");
-}, 3000);
+var stdin = process.openStdin();
+
+var data = "";
+
+stdin.on('data', function(chunk) {
+	data += chunk;
+	console.log("data");
+});
+
+stdin.on('end', function() {
+	console.log("DATA:\n" + data + "\nEND DATA");
+});
