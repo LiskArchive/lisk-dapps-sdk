5e86de6d265ce3261a512cc934d7fe092131c908
Boris Povod <boris@crypti.me>
2015-07-12 10:22:38 +0300

    crypto api

--- api/openAccount.js
+++ api/openAccount.js
@@ -1,7 +1,7 @@
 module.exports = function (body, crypti, cb) {
 	crypti.accounts.open(body.secret, function (err, account) {
 		if (err) {
-			return cb(null, {err: err});
+			return cb(null, {error: err});
 		} else {
 			return cb(null, {account: account});
 		}
