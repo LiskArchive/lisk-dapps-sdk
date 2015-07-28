25b59915acdeda2452a3b9dc71a370c838030dac
Boris Povod <boris@crypti.me>
2015-07-28 15:45:12 +0300

    comment

--- modules/blockchain/hash.js
+++ modules/blockchain/hash.js
@@ -10,6 +10,9 @@ function Hash(cb, library) {
 
 Hash.prototype.createHash = function (delegate, cb) {
 	var unconfirmedList = private.modules.data.getUnconfirmedList();
+
+	// process array of transactions
+
 	private.modules.transport.message("hash", {"hash": "", "sign": ""})
 }
 
