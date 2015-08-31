664acd8d5359f7d640e7ed400815d55614e228d6
Pavel <landgraf.paul@gmail.com>
2015-09-01 01:30:45 +0200

    sync

--- modules/system/sync.js
+++ modules/system/sync.js
@@ -1,3 +1,5 @@
+var bignum = require('browserify-bignum');
+
 var private = {}, self = null,
 	library = null, modules = null;
 
@@ -7,10 +9,38 @@ function Sync(cb, _library) {
 	cb(null, self);
 }
 
+private.findUpdate = function (lastBlock, peer, cb) {
+	modules.api.transport.getRandomPeer(peer, "get", "/blocks/common", {lastId: lastBlock.height}, function (err, res) {
+		if (err || res.body.success) {
+			return cb(err);
+		}
+
+		if (lastBlock.height - res.body.response.commonBlock.height > 1440) {
+			return cb();
+		}
+
+		modules.api.transport.getRandomPeer(peer, "get", "/blocks", {commonId: res.body.response.commonBlock.height}, function (err, res) {
+			async.eachSeries(res.body.response.blocks, function (block, cb) {
+
+			});
+		});
+
+	});
+}
+
 private.blockSync = function (lastBlock, cb) {
 	modules.api.transport.getRandomPeer("get", "/blocks/height", null, function (err, res) {
-		modules.api.transport.getPeer(res.peer, "get", "/blocks/height", null, function (err, res) {
-		});
+		if (res.body.success) {
+			modules.blockchain.blocks.getLastBlock(function (err, lastBlock) {
+				if (bignum(lastBlock.height).lt(res.body.response.height)) {
+					private.findUpdate(lastBlock, data.peer, cb);
+				} else {
+					setImmediate(cb);
+				}
+			});
+		} else {
+			setImmediate(cb);
+		}
 	});
 }
 
